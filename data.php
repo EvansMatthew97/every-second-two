9874566267d6bb8becf800f82b89ed1f074b7d88
MrPutuLips <evans.matthew97@gmail.com>
2016-09-24 10:57:43 +0200

    Fixed scroll overflow in modal on IE

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ $data = [
 	['title'=> 'Lung disease', 'num'=> 3.1, 'url'=> 'Respiratory_disease', 'colour' => '#E77E7E'],
 	['title'=> 'Influenza and Pneumonia', 'num'=> 3.05, 'url'=> 'Influenza', 'colour' => '#FB7'],
 	['title'=> 'HIV/AIDS', 'num'=> 1.533, 'url'=> 'HIV/AIDS', 'colour' => '#E77E7E'],
-	['title'=> 'Diarrhoeal diseases', 'num'=> 1.4977, 'url'=> 'Diarrhea', 'colour' => '#D7E283'],
+	['title'=> 'Diarrhoeal diseases', 'num'=> 1.4977, 'url'=> 'Diarrhea', 'colour' => '#B7A283'],
 	['title'=> 'Diabetes Mellitus', 'num'=> 1.4973, 'url'=> 'Diabetes_mellitus', 'colour' => '#7EA1E7'],
 	['title'=> 'Road traffic accidents', 'num'=> 1.254, 'url'=> 'Traffic_collision', 'colour' => '#E77EC6'],
 	['title'=> 'Hypertension', 'num'=> 1.141, 'url'=> 'Hypertension', 'colour' => '#EE9377'],
